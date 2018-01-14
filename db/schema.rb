45a1b30d011b7b47d45f9fcb757624f28cad6375
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-01-13 19:21:35 -0500

    added comment model and linked to recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113232027) do
+ActiveRecord::Schema.define(version: 20180114002028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -59,4 +67,5 @@ ActiveRecord::Schema.define(version: 20180113232027) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "recipes"
 end
