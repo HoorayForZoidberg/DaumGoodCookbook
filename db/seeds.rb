aca46743551fbc510e26b78a6b51f88d65864fe0
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-01-14 19:09:53 -0500

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ Ingredient.destroy_all
 
 # ingredients seed
 puts "Creating ingredients ..."
-ingredients = %w(butter, sugar, flour, egg, oil, vinegar, onion, garlic, salt, pepper, chicken breasts, ground beef, bread, tomato, salad, corn, potato, sweet potato, green pepper, red pepper, green beans, black beans)
+ingredients = %w(butter sugar flour egg oil vinegar onion garlic salt pepper chicken beef bread tomato salad corn potato beans)
 
 ingredients.each do |ingredient|
   Ingredient.create({name: ingredient})
@@ -24,7 +24,7 @@ end
 
 # measures seed
 puts "Creating measures ..."
-measures = %w(ounce, pinch, gram, kilo, pound, teaspoon, tablespoon, cup, quart, gallon, dash)
+measures = %w(ounce pinch gram kilo pound teaspoon tablespoon cup quart gallon dash)
 
 measures.each do |measure|
   Measure.create({name: measure})
@@ -33,7 +33,7 @@ end
 
 # recipes seed
 puts "Creating recipes ..."
-recipes = %w(gloob, blarg, mlurp, prums)
+recipes = %w(gloob blarg mlurp prums)
 
 recipes.each do |recipe|
   Recipe.create({name: recipe, summary: "Recipe for #{recipe}"})
