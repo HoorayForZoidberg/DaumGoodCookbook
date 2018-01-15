01e4a8e4f93afbd851a18014713c2663e562462c
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-01-15 18:31:58 -0500

    updated schema after adding owner column to recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180114225239) do
+ActiveRecord::Schema.define(version: 20180115232428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20180114225239) do
     t.string "summary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "owner_id"
+    t.index ["owner_id"], name: "index_recipes_on_owner_id"
   end
 
   create_table "steps", force: :cascade do |t|
