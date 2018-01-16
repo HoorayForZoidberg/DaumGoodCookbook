acdf20bdf0bce12647893a5229be716c03c30ab2
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-01-15 19:17:14 -0500

    added owner method in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   has_many :steps, dependent: :destroy
   has_many :ingredients
   has_many :comments, dependent: :destroy
+
+  def owner
+    return User.find(self.owner_id).name
+  end
 end
