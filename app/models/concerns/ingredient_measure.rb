72feca65d381d81c6f4e77139944c621cb1a2e32
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-01-24 22:54:08 -0500

    added skeleton for ingredients to the view (still need input form)

--- app/models/concerns/ingredient_measure.rb
+++ app/models/concerns/ingredient_measure.rb
@@ -5,9 +5,9 @@ module IngredientMeasure
 
   #Load
   def load
-      measures.each do |measure|
-          proxy_association.target << measure
-      end
+    measures.each do |measure|
+      proxy_association.target << measure
+    end
   end
 
   #Private
@@ -15,13 +15,13 @@ module IngredientMeasure
 
   #measures
   def measures
-      return_array = []
-      through_collection.each_with_index do |through,i|
-          associate = through.send(reflection_name)
-          associate.assign_attributes({measure: items[i]}) if items[i].present?
-          return_array.concat Array.new(1).fill( associate )
-      end
-      return_array
+    return_array = []
+    through_collection.each_with_index do |through,i|
+      associate = through.send(reflection_name)
+      associate.assign_attributes({measure: items[i]}) if items[i].present?
+      return_array.concat Array.new(1).fill( associate )
+    end
+    return_array
   end
 
   #######################
@@ -30,37 +30,37 @@ module IngredientMeasure
 
   #Association
   def reflection_name
-      proxy_association.source_reflection.name
+    proxy_association.source_reflection.name
   end
 
   #Foreign Key
   def through_source_key
-      proxy_association.reflection.source_reflection.foreign_key
+    proxy_association.reflection.source_reflection.foreign_key
   end
 
   #Primary Key
   def through_primary_key
-      proxy_association.reflection.through_reflection.active_record_primary_key
+    proxy_association.reflection.through_reflection.active_record_primary_key
   end
 
   #Through Name
   def through_name
-      proxy_association.reflection.through_reflection.name
+    proxy_association.reflection.through_reflection.name
   end
 
   #Through
   def through_collection
-      proxy_association.owner.send through_name
+    proxy_association.owner.send through_name
   end
 
   #Captions
   def items
-      through_collection.map(&:measure)
+    through_collection.map(&:measure)
   end
 
   #Target
   def target_collection
-      #load_target
-      proxy_association.target
+    #load_target
+    proxy_association.target
   end
 end
