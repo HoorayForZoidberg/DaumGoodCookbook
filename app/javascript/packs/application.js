2f957c8a54bce43426eb4593705f8eb20da67941
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-04-06 17:39:12 -0400

    navbar sticks to top, avatar menu drops down, changed some formatting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
 import "bootstrap";
+
+// Navbar avatar dropwdown menu toggle
+$(document).ready(function(){
+  $( "#navbar-wagon-menu" ).click(function() {
+    $('.dropdown-toggle').dropdown();
+  });
+})
