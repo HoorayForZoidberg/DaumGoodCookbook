c89283020f905f502c219ac74a89bf266178b92d
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-04-07 13:47:19 -0400

    title transfers to navbar with JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,25 @@ import "bootstrap";
 
 // Navbar avatar dropwdown menu toggle
 $(document).ready(function(){
-  $( "#navbar-wagon-menu" ).click(function() {
+  $("#navbar-wagon-menu").click(function() {
     $('.dropdown-toggle').dropdown();
   });
 })
+
+// Add recipe title to navbar
+$(document).ready(function(){
+  if ($("#recipe-title")) {
+    const title = $("#recipe-title>div").html();
+    $("#navbar-recipe-title").html(title);
+  }
+})
+
+// toggle hidden class on title and navbar title after enough upcscroll
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 500) {
+    $('#navbar-recipe-title').removeClass("hidden");
+  } else {
+    $('#navbar-recipe-title').addClass("hidden");
+  }
+});
