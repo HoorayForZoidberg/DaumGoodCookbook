eee65701cac4c6b560f23443271445e34698e478
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-04-10 17:01:15 -0400

    fixed partial rendering on new.js, added cancel button to new ingredient form

--- app/views/steps/new.js
+++ app/views/steps/new.js
@@ -1,1 +1,10 @@
-$('#steps').append('<%= j render "form", locals: {recipe: @recipe, step: @step} %>');
+// Display step addition menu when plus button is clicked
+$(".new-step .new-step-form").removeClass("hidden");
+$(".new-step a").addClass("hidden");
+
+// Remove step-addition menu when cancel button is clicked
+$(".cancel-step").click(function(){
+  $(".new-step .new-step-form").addClass("hidden");
+  $(".new-step a").removeClass("hidden");
+});
+
