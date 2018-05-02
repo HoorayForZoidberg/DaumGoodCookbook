e1c92828af657b047f49cffc98b397e20ed1acc7
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-05-02 16:45:01 -0400

    fixed invalid foreign key reference in user recipe comments  models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :recipes, foreign_key: "owner_id"
+  has_many :comments, foreign_key: "owner_id"
 end
