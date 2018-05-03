cfd38021940398b7f045d38c52b7ddc2b5452a19
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-05-02 23:45:06 -0400

    various css and JS fixes - could not implement passive event handler with jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,30 +10,28 @@ import swal from 'sweetalert'
 
 
 // RECIPE PAGE
+if ($("#recipe")) {
 
-$(document).ready(function(){
-  if ($("#recipe")) {
+  // Add hidden recipe title to navbar
+  const title = $("#recipe-title>h1").html();
+  $("#navbar-recipe-title").html(`<h1>${title}</h1>`);
 
-    // Add hidden recipe title to navbar
-    const title = $("#recipe-title>h1").html();
-    $("#navbar-recipe-title").html(`<h1>${title}</h1>`);
+  // toggle hidden class on title and navbar title after enough upcscroll
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 500) {
+      $('#navbar-recipe-title').removeClass("hidden");
+    } else {
+      $('#navbar-recipe-title').addClass("hidden");
+    }
+  });
 
-    // toggle hidden class on title and navbar title after enough upcscroll
-    $(document).scroll(function() {
-      var y = $(this).scrollTop();
-      if (y > 500) {
-        $('#navbar-recipe-title').removeClass("hidden");
-      } else {
-        $('#navbar-recipe-title').addClass("hidden");
-      }
-    });
+  // reveal edit buttons when "edit this recipe" is clicked
+  $(".edit-mode").click(function(e){
+    // e.preventDefault();
+    $(".edit-mode a").toggleClass("hidden");
+    $(".editable:not(.comments-container)").toggleClass("hidden");
+  });
+}
 
-    // reveal edit buttons when "edit this recipe" is clicked
-    $(".edit-mode").click(function(e){
-      e.preventDefault();
-      $(".edit-mode a").toggleClass("hidden");
-      $(".editable:not(.comments-container)").toggleClass("hidden");
-    });
-  }
-})
 
