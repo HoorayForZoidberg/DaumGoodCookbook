4d6a0e2644db44255f26b0576418711c62e50b68
Nicolas Lizop <nicolas.lizop@gmail.com>
2019-07-04 01:25:19 -0400

    not yet there but soon

--- config/initializers/serviceworker.rb
+++ config/initializers/serviceworker.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.serviceworker.routes.draw do
-    match "/serviceworker.js"
-    match "/manifest.json"
+    match "/serviceworker.js" => "app/assets/javascripts/serviceworker.js"
+    match "/manifest.json" => "app/assets/javascripts/manifest.json"
   end
 end
